492d651c3c895f035e27582771083640aa9d1870
Andrew McLagan <andrew@ethicaljobs.com.au>
2018-08-06 16:13:08 +1000

    Rollbar service tests.

--- tests/Integration/ServiceProviders/RollbarServiceProviderTest.php
+++ tests/Integration/ServiceProviders/RollbarServiceProviderTest.php
@@ -4,41 +4,63 @@ namespace Tests\Integration\ServiceProviders;
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
+use EthicalJobs\Foundation\Laravel\LoggingServiceProvider;
 
 class RollbarServiceProviderTest extends \Tests\TestCase
 {
-    // Currently cant set the environment during testing to check this.
-    // 
-    // /**
-    //  * @test
-    //  * @group Unit
-    //  */
-    // public function it_loads_rollbar_service_provider_in_correct_envs()
-    // {
-    //     $providers = $this->app->getLoadedProviders();
+    /**
+     * @test
+     */
+    public function it_loads_rollbar_service_provider_in_production()
+    {        
+        App::shouldReceive('environment')->andReturn('production');
+        
+        $this->app->register(LoggingServiceProvider::class, [], true);
+        
+        $providers = $this->app->getLoadedProviders();
 
-    //     $this->assertTrue($providers['Rollbar\Laravel\RollbarServiceProvider']);
-    // }     
-
-    // /**
-    //  * @test
-    //  * @group Unit
-    //  */
-    // public function it_loads_package_config()
-    // {
-    //     $config = config('services');
-
-    //     $this->assertTrue(array_has($config, 'rollbar'));
-
-    //     $this->assertTrue(array_has($config, 'rollbar.access_token'));
-    // }     
+        $this->assertTrue($providers['Rollbar\Laravel\RollbarServiceProvider']);    
+    }     
 
     /**
      * @test
-     * @group Unit
      */
-    public function it_returns_true()
+    public function it_loads_rollbar_service_provider_in_staging()
+    {        
+        App::shouldReceive('environment')->andReturn('staging');
+        
+        $this->app->register(LoggingServiceProvider::class, [], true);
+        
+        $providers = $this->app->getLoadedProviders();
+
+        $this->assertTrue($providers['Rollbar\Laravel\RollbarServiceProvider']);       
+    }       
+
+    /**
+     * @test
+     */
+    public function it_does_not_load_rollbar_service_provider_in_other_envs()
+    {        
+        $this->app->register(LoggingServiceProvider::class, [], true);
+        
+        $providers = $this->app->getLoadedProviders();
+
+        $this->assertFalse(isset($providers['Rollbar\Laravel\RollbarServiceProvider']));
+
+        $this->assertEquals('testing', $this->app->environment());
+    }           
+
+    /**
+     * @test
+     */
+    public function it_loads_package_config()
     {
-        $this->assertTrue(true);
-    }      
+        $this->app->register(LoggingServiceProvider::class, [], true);
+
+        $config = config('services');
+
+        $this->assertTrue(array_has($config, 'rollbar'));
+
+        $this->assertTrue(array_has($config, 'rollbar.access_token'));
+    }     
 }
